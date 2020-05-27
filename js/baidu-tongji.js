9bd59674e6e01f167dabe806a3d9f392fe72c2a4
HPU-Yz <2426545812@qq.com>
2020-05-27 15:01:55 +0800

    Site updated: 2020-05-27 15:01:54

--- js/baidu-tongji.js
+++ js/baidu-tongji.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?";
+  hm.src = "https://hm.baidu.com/hm.js?72b1e5dff8c09a3e4acdfefe76e1b51a";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
